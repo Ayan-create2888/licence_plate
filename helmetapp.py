38fd605117b1297c4076e0dfefad4e710ee442e8
Ayan-create2888 <3513ayaanbaba@gmail.com>
2025-12-29 21:16:33 +0500

    Update helmetapp.py

--- helmetapp.py
+++ helmetapp.py
@@ -7,8 +7,6 @@ from PIL import Image
 
 # ---------------- LOAD MODEL ----------------
 model = YOLO("best_helmet.pt")
-st.write("OpenCV imported successfully")
-
 
 # ---------------- STREAMLIT CONFIG ----------------
 st.set_page_config(page_title="🪖 Helmet Detection", layout="centered")
@@ -118,4 +116,5 @@ if uploaded_file is not None:
                 file_name="helmet_detected.mp4",
                 mime="video/mp4"
             )
+
 
